0c7157e51be24049176628c2c329d780d277bcb8
vienkimlan0405 <vienkimlan0405@gmail.com>
2026-07-31 10:26:08 +0700

    Create Student.java

---
+++ 1. Hello Entity/Entity/Student.java
@@ -0,0 +1,24 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "students")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(length = 100)
+    private String major;
+}
