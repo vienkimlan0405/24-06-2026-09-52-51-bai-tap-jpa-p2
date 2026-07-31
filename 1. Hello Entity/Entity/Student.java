fbd046c795af81d4f296b3a44ee83dfe03049470
vienkimlan0405 <vienkimlan0405@gmail.com>
2026-07-31 10:35:32 +0700

    Update Student.java

--- 1. Hello Entity/Entity/Student.java
+++ 1. Hello Entity/Entity/Student.java
@@ -13,12 +13,6 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 100)
     private String name;
-
-    @Column(nullable = false, unique = true, length = 100)
     private String email;
-
-    @Column(length = 100)
-    private String major;
 }
