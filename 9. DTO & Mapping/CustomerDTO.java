4f0400f9494e3661419cf2c58ded53ccbe224b51
vienkimlan0405 <vienkimlan0405@gmail.com>
2026-07-31 11:06:23 +0700

    Update CustomerDTO.java

--- 9. DTO & Mapping/CustomerDTO.java
+++ 9. DTO & Mapping/CustomerDTO.java
@@ -5,8 +5,8 @@ import lombok.*;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class CustomerDTO { //[cite: 2]
-    private Long id; //[cite: 2]
-    private String name; //[cite: 2]
-    private String phone; // Bỏ trường email để ẩn thông tin sensitive[cite: 2]
+public class CustomerDTO {
+    private Long id;
+    private String name;
+    private String phone; // Bỏ trường email để ẩn thông tin sensitive
 }
