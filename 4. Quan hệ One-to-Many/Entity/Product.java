5a35a28db62c7f4f40405527e64e3e2d5496ced8
vienkimlan0405 <vienkimlan0405@gmail.com>
2026-07-31 10:47:22 +0700

    Create Product.java

---
+++ 4. Quan hệ One-to-Many/Entity/Product.java
@@ -0,0 +1,23 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
