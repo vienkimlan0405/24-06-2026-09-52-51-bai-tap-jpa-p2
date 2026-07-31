b6a0c41c0c7e4b2562dd090a533cdf5b5711c6dd
vienkimlan0405 <vienkimlan0405@gmail.com>
2026-07-31 10:28:16 +0700

    Create ProductRepository.java

---
+++ 2. CRUD Product/Repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    // Tìm kiếm theo tên có phân trang
+    Page<Product> findByNameContainingIgnoreCase(String name, Pageable pageable);
+}
